c8acaa22c7a49a347b562246e9f9924d38e08f23
armita-teymuri <armita.teymuri71@gmail.com>
2022-11-07 20:25:11 +0330

    update

---
+++ course/JQuery session 2/main01.py
@@ -0,0 +1,71 @@
+import numpy as np
+
+D = np.array([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0],
+])
+
+
+L = [8, 8, 8, 8, 8, 8, 8, 8, 8]
+
+
+X = {
+    0: '-',
+    1: '@',
+    2: '#',
+}
+
+p1, p2 = 1, 2
+
+
+def show():
+    for row in D:
+        for cell in row:
+            print(X[cell], end="")
+            # print(cell, end="")
+        print()
+
+
+def check_4(row):
+    if (np.sum(row) == 4 and not np.any(row == 2)):
+        print("@ is the winner")
+        exit()
+    if (np.sum(row) == 8):
+        print("# is the winner")
+        exit()
+
+
+def check_4_square(M):
+    for x in [0, 1, 2, 3]:
+        check_4(M[x])
+        check_4(M[:, x])
+    check_4(np.array([M[0, 0], M[1, 1], M[2, 2], M[3, 3]]))
+    check_4(np.array([M[0, 3], M[1, 2], M[2, 1], M[3, 0]]))
+
+
+def check_winner():
+    for row in range(0, 6):
+        for col in range(0, 6):
+            #  Div and Conq
+            M = D[row:row+4, col:col+4]
+            # print(M)
+            check_4_square(M)
+
+
+show()
+while True:
+    i = int(input(f"Enter 1-9: "))
+    if i >= 1 and i <= 9:
+        idx = i-1
+        D[L[idx]][idx] = p1
+        L[idx] -= 1
+        p1, p2 = p2, p1
+        show()
+        check_winner()
--- homeWork/dictionary/server.php
+++ homeWork/dictionary/server.php
@@ -1,14 +1,17 @@
 <?php
 
+xdebug_break();
 $word = $_GET["word"];
-$translate =$_GET["translate"];
+$translate =$_GET["translate"]; 
 $arr = array(
     "hello"     =>  "سلام",
     "hi"        =>  "سلام",
     "goodbye"   =>  "خداحافظ",
     "bye"       =>  "خداحافظ",
 );
-print_r($word);
+
+xdebug_break();
+//print_r($word);
 if($word == ""){
     echo file_get_contents("./index.html");
 }elseif( $word != ""  &  $translate == ""){
